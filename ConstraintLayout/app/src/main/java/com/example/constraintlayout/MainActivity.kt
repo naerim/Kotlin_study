2669ec61cee699534f091f1daca01b1660465ad4
naerim <naerim1119@gmail.com>
2021-01-14 01:09:22 +0900

    ConstraintLayout practice

--- ConstraintLayout/app/src/main/java/com/example/constraintlayout/MainActivity.kt
+++ ConstraintLayout/app/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -7,19 +7,19 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 //    lateinit : 나중에 초기화할 것이라는 것을 알려준다
-    lateinit var textView : TextView
-    lateinit var clickButton : Button
+//    lateinit var textView : TextView
+//    lateinit var clickButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView = findViewById(R.id.textView)
-        clickButton = findViewById(R.id.clickButton)
+//        textView = findViewById(R.id.textView)
+//        clickButton = findViewById(R.id.clickButton)
 
 //        클릭 이벤트
-        clickButton.setOnClickListener {
-            textView.text = "버튼을 눌려주세요"
-        }
+//        clickButton.setOnClickListener {
+//            textView.text = "버튼을 눌려주세요"
+//        }
     }
 }
