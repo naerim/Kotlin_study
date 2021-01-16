77df1272d14a0f1ba177bd654751de24a841b0da
naerim <naerim1119@gmail.com>
2021-01-17 03:09:12 +0900

    결과 화면 작성, 인텐트로 화면 전환

--- BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.bmicalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    // 추후에 초기화 변수타입
+    lateinit var resultButton : Button
+    lateinit var heightEditText: EditText
+    lateinit var weightEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resultButton = findViewById<Button>(R.id.resultButton)
+        heightEditText = findViewById<EditText>(R.id.heightEditText)
+        weightEditText = findViewById<EditText>(R.id.weightEditText)
+
+        resultButton.setOnClickListener {
+            var intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("height", heightEditText.text.toString())
+            intent.putExtra("weight", weightEditText.text.toString())
+            startActivity(intent)
+        }
     }
 }
