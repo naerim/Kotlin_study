f6dc3d7bd03ad9c760db27ca90fb0809e8feca70
naerim <naerim1119@gmail.com>
2021-01-17 15:03:53 +0900

    공유 환경 설정(SharedPreference)으로 데이터 저장

--- BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -15,16 +15,40 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         resultButton = findViewById<Button>(R.id.resultButton)
         heightEditText = findViewById<EditText>(R.id.heightEditText)
         weightEditText = findViewById<EditText>(R.id.weightEditText)
 
+        // 실행하자마자 값이 제대로 셋팅됨
+        loadData()
+
         resultButton.setOnClickListener {
+            // 버튼을 누르는 순간 저장된다.
+            saveData(heightEditText.text.toString().toInt(),
+                     weightEditText.text.toString().toInt())
             var intent = Intent(this, ResultActivity::class.java)
             intent.putExtra("height", heightEditText.text.toString())
             intent.putExtra("weight", weightEditText.text.toString())
             startActivity(intent)
+        }
+    }
+
+    private fun saveData(height : Int, weight: Int){
+        var pref = this.getPreferences(0)
+        var editor = pref.edit()
+
+        editor.putInt("KEY_HEIGHT", heightEditText.text.toString().toInt()).apply()
+        editor.putInt("KEY_WEIGHT", weightEditText.text.toString().toInt()).apply()
+    }
+
+    private fun loadData(){
+        var pref = this.getPreferences(0)
+        var height = pref.getInt("KEY_HEIGHT",0)
+        var weight = pref.getInt("KEY_WEIGHT",0)
+
+        if(height != 0 && weight != 0){
+            heightEditText.setText(height.toString())
+            weightEditText.setText(weight.toString())
         }
     }
 }
