09adf8a3de175752de2e5124927465b3976a0bba
naerim <naerim1119@gmail.com>
2021-01-17 18:40:23 +0900

    타이머 구현

--- StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+
+    private var time = 0
+    private var timerTask : Timer? = null
+    var isRunning = false
+
+    lateinit var fab : FloatingActionButton
+    lateinit var secTextView : TextView
+    lateinit var milliTextView : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fab = findViewById<FloatingActionButton>(R.id.fab)
+        secTextView = findViewById(R.id.secTextView)
+        milliTextView = findViewById(R.id.milliTextView)
+
+        fab.setOnClickListener {
+            isRunning = !isRunning
+            if(isRunning){
+                start()
+            } else{
+                pause()
+            }
+        }
+    }
+
+    private fun pause() {
+        fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+        timerTask?.cancel()
+    }
+
+    private fun start() {
+        fab.setImageResource(R.drawable.ic_baseline_pause_24)
+
+        timerTask = timer(period = 10){
+            time++
+            val sec = time / 100
+            val milli = time % 100
+            runOnUiThread{
+                secTextView.text = "$sec"
+                milliTextView.text = "$milli"
+            }
+        }
     }
 }
