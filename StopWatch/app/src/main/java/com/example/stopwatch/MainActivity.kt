c8292ff76a71b69f8bcbc452a79e239a32dacfb5
naerim <naerim1119@gmail.com>
2021-01-18 15:17:22 +0900

    타이머 설정 기능, Done!

--- StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.stopwatch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -16,54 +17,40 @@ class MainActivity : AppCompatActivity() {
     private var isRunning = false
     private var lap = 1
 
-    lateinit var fab : FloatingActionButton
     lateinit var secTextView : TextView
     lateinit var milliTextView : TextView
-    lateinit var labLayout : LinearLayout
-    lateinit var labButton: Button
-    lateinit var resetFab : FloatingActionButton
+    lateinit var secEditText: EditText
+    lateinit var setButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fab = findViewById<FloatingActionButton>(R.id.fab)
         secTextView = findViewById<TextView>(R.id.secTextView)
         milliTextView = findViewById<TextView>(R.id.milliTextView)
-        labLayout = findViewById<LinearLayout>(R.id.labLayout)
-        labButton = findViewById<Button>(R.id.labButton)
-        resetFab = findViewById<FloatingActionButton>(R.id.resetFab)
+        secEditText = findViewById<EditText>(R.id.secEditText)
+        setButton = findViewById<Button>(R.id.setButton)
 
-        fab.setOnClickListener {
-            isRunning = !isRunning
-            if(isRunning){
-                start()
-            } else{
-                pause()
-            }
-        }
-
-        labButton.setOnClickListener {
-            recordLapTime()
-        }
-
-        resetFab.setOnClickListener {
-            reset()
+        setButton.setOnClickListener {
+            time = secEditText.text.toString().toInt()*100
+            start()
         }
     }
 
     private fun pause() {
-        fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+        //fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
         timerTask?.cancel()
     }
 
     private fun start() {
-        fab.setImageResource(R.drawable.ic_baseline_pause_24)
+        //fab.setImageResource(R.drawable.ic_baseline_pause_24)
 
         timerTask = timer(period = 10){
-            time++
+            time--
             val sec = time / 100
             val milli = time % 100
+
+            if(sec==0 && milli==0) timerTask?.cancel()
             runOnUiThread{
                 secTextView.text = "$sec"
                 milliTextView.text = "$milli"
@@ -71,26 +58,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun recordLapTime() {
-        val lapTime = this.time
-        val textView = TextView(this)
-        textView.text = "$lap LAB : ${lapTime/100}.${lapTime%100}"
-        
-        // 값 추가시 위에 부터 (cf lap 값을 넣으면 밑에서 부터)
-        labLayout.addView(textView, 0)
-        lap++
-    }
 
     private fun reset() {
         timerTask?.cancel()
 
         time = 0
         isRunning = false
-        fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+        //fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
         secTextView.text = "0"
         milliTextView.text = "00"
 
-        labLayout.removeAllViews()
+        //labLayout.removeAllViews()
         lap = 1
     }
 }
