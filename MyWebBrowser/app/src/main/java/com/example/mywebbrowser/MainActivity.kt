f62e22326f9498ad0d8178cc424a3b4ed9975612
naerim <naerim1119@gmail.com>
2021-01-21 13:45:12 +0900

    화면 작성, 기본 웹 브라우저 기능 작성, 암시적 인텐트

--- MyWebBrowser/app/src/main/java/com/example/mywebbrowser/MainActivity.kt
+++ MyWebBrowser/app/src/main/java/com/example/mywebbrowser/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.mywebbrowser
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -25,6 +31,9 @@ class MainActivity : AppCompatActivity() {
 
         webView.loadUrl("http://www.google.com")
 
+        // context 메뉴(화면 길게 클릭하면 나옴)
+        registerForContextMenu(webView)
+
         urlEditText.setOnEditorActionListener { v, actionId, event ->
             // 검색 버튼 눌렀을 때
             if(actionId == EditorInfo.IME_ACTION_SEARCH){
@@ -45,5 +54,79 @@ class MainActivity : AppCompatActivity() {
         else {
             super.onBackPressed()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.action_google, R.id.action_home -> {
+                webView.loadUrl("http://www.google.com")
+                return true
+            }
+            R.id.action_naver -> {
+                webView.loadUrl("http://www.naver.com")
+                return true
+            }
+            R.id.action_daum -> {
+                webView.loadUrl("http://www.daum.net")
+                return true
+            }
+            R.id.action_call -> {
+                // 전화하기
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:012-3456-7890")
+                if(intent.resolveActivity(packageManager)!=null){
+                    startActivity(intent)
+                }
+                return true
+            }
+            R.id.action_send_text -> {
+                // 문자보내기
+                val intent = Intent(Intent.ACTION_SENDTO)
+                intent.data = Uri.parse("smsto:" + Uri.encode("012-3456-7890"))
+                if(intent.resolveActivity(packageManager)!=null){
+                    startActivity(intent)
+                }
+                return true
+            }
+            R.id.action_email -> {
+                // 이메일하기
+                val intent = Intent(Intent.ACTION_SENDTO)
+                intent.data = Uri.parse("mailto:example@example.com")
+                if(intent.resolveActivity(packageManager)!=null){
+                    startActivity(intent)
+                }
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.context, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.action_share -> {
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.putExtra(Intent.EXTRA_TEXT, webView.url.toString())
+                intent.setType("text/plain")
+                val shareIntent = Intent.createChooser(intent, "공유 페이지")
+                startActivity(shareIntent)
+                return true
+            }
+            R.id.action_browser -> {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(webView.url))
+                startActivity(Intent.createChooser(intent, "Browser"))
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
     }
 }
