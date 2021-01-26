5559b1a5fa22aa49a8f319bf80cdf846f758e102
naerim <naerim1119@gmail.com>
2021-01-26 18:31:55 +0900

    실로폰 프로젝트 구성(가로 화면 고정, 액션바 제거)

---
+++ Xylophone/app/src/main/java/com/example/xylophone/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.xylophone
+
+import android.content.pm.ActivityInfo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // 화면이 가로 모드로 고정되게 하기
+        // requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
